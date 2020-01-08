23bedcc2be74603ea829e443475a99da6dbb0ab1
ReturnSweetPork <happylovetkd@naver.com>
2020-01-08 23:17:29 +0900

    PropsType

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import MyComponent from "./MyComponent";
 
 const App = () => {
-  return <MyComponent>칠드런</MyComponent>;
+  return (
+    <MyComponent name="안현상" favoriteNumber={77}>
+      칠드런
+    </MyComponent>
+  );
 };
 
 export default App;
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,11 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const MyComponent = ({ name, children }) => {
+const MyComponent = ({ name, children, favoriteNumber }) => {
   return (
     <div>
       안녕하세요 제이름은{name}
       <br />
       칠드런은{children}
+      <br />
+      제가 가장 좋아하는 숫자는 {favoriteNumber}
     </div>
   );
 };
@@ -14,4 +17,9 @@ export default MyComponent;
 
 MyComponent.defaultProps = {
   name: "기본이름"
+};
+
+MyComponent.propTypes = {
+  name: PropTypes.string,
+  favoriteNumber: PropTypes.number.isRequired
 };
